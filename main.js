dcad15725e8858387f3956ee94ed5c775af44dfe
egpennington <egpennington@hotmail.com>
2024-10-23 12:41:52 -0800

    add more styles and js for color box of user input

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
-function clear() {
+let inputColor = document.getElementById("input-color").value
+const userInputHex = document.querySelector(".user-input-hex")
+
+function clearScheme() {
     document.getElementById("color-scheme-show").innerHTML = ""
+
 }
 
+document.getElementById("input-color").addEventListener("input", function() {
+    inputColor = this.value
+    userInputHex.style.backgroundColor = `#${inputColor}`
+});
+
 document.querySelector("button").addEventListener("click", function() {
-    console.log("clicked")
-    clear()
-    const inputColor = document.getElementById("input-color").value
-    let choice = document.getElementById("choices").value
-    
+    clearScheme()    
+    const choice = document.getElementById("choices").value; 
+        
     fetch(`https://www.thecolorapi.com/scheme?hex=${inputColor}&mode=${choice}&count=6`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data)
             const colors = data.colors
             
             colors.forEach(color => {
-                console.log(color.hex.value, color.image.bare)
                 document.getElementById("color-scheme-show").innerHTML += `
                     <div class="color-scheme-content">
                         <img src='${color.image.bare}' alt='Color Image' class='image'>
-                        <p id="color-hex-copy">${color.hex.value}</p>
+                        <p id="color-hex-copy" style="color: ${color.hex.value}">${color.hex.value}</p>
                     </div>
-                `
+                `            
             })
         })
 })
-
-
-// copy hex codes
-document.getElementById(`color-hex-copy-${index}`).addEventListener('click', function() {
-	   navigator.clipboard.writeText(color.hex.value)
-               	.then(() => {
-                    alert('Hex code copied to clipboard!');
-                })
-		.catch(err => {
-                    console.error('Failed to copy text: ', err);
-                });
-            });
