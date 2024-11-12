0b95a1519359b76290ada276e80c055010d1590f
egpennington <egpennington@hotmail.com>
2024-11-12 10:43:13 -0900

    changed to copy hex codes

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const userInputHex = document.querySelector(".user-input-hex")
 
 function clearScheme() {
     document.getElementById("color-scheme-show").innerHTML = ""
-
 }
 
 document.getElementById("input-color").addEventListener("input", function() {
@@ -12,21 +11,44 @@ document.getElementById("input-color").addEventListener("input", function() {
 });
 
 document.querySelector("button").addEventListener("click", function() {
-    clearScheme()    
-    const choice = document.getElementById("choices").value; 
-        
+    clearScheme();    
+    const choice = document.getElementById("choices").value
+    
     fetch(`https://www.thecolorapi.com/scheme?hex=${inputColor}&mode=${choice}&count=6`)
         .then(res => res.json())
         .then(data => {
             const colors = data.colors
             
+            // Clear any existing content before appending new colors
+            document.getElementById("color-scheme-show").innerHTML = ''
+            
             colors.forEach(color => {
                 document.getElementById("color-scheme-show").innerHTML += `
                     <div class="color-scheme-content">
                         <img src='${color.image.bare}' alt='Color Image' class='image'>
-                        <p id="color-hex-copy" style="color: ${color.hex.value}">${color.hex.value}</p>
+                        <p class="color-code">${color.hex.value}</p>
                     </div>
-                `            
+                `           
             })
+            
+            // copy the color code
+            document.querySelectorAll('.color-code').forEach(element => {
+                element.addEventListener('click', function() {
+                    const originalText = this.textContent; // Save the original hex value
+                    navigator.clipboard.writeText(originalText).then(() => {
+                        this.textContent = "Copied"; // Change text to "Copied"
+            
+                        // Revert back to the original hex value after 1 second
+                        setTimeout(() => {
+                            this.textContent = originalText;
+                        }, 1000);
+                    }).catch(err => {
+                        console.error('Failed to copy text: ', err);
+                    });
+                });
+            });
+            
         })
+        .catch(err => console.error('Error fetching color scheme:', err))
 })
+
